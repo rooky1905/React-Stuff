a9694d29ae7ca29f8d921389f611db749681374a
rooky1905 <ritikgarg1975@gmail.com>
2021-09-01 01:57:06 +0530

    Some components info updated with minor tweaks

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <Pokedex player = {1}/>
-      <Pokedex player = {2}/> */}
-      {/* <Rando /> */}
-      <DiceGame />
+      {/* For Dice Game (Involves, State as Props, Props, Transitions, and much more complex) */}
+        <DiceGame />
+
+      {/* For Pokedex project (Basic Props and stuff) */}
+        {/* <Pokedex player = {1}/>
+        <Pokedex player = {2}/> */}
+
+      {/* For Random number generator gamr (Involves States, Props) */}
+        {/* <Rando /> */}
+
     </div>
   );
 }
